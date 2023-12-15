f966136b05aecf3dc499f75c7cb72addab1ad76f
Alexey <lisin9208@gmail.com>
2023-12-15 19:09:17 +0700

    решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = ["Hello", "2", "world", ":-)"];
+string[] NewArray = new string[array.Length];
+
+int symbol = 3;    // количество необходимых символов в строке
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= symbol)
+    {
+        NewArray[i] = array[i];
+    }
+}
+var str = string.Join(" ", NewArray);
+Console.WriteLine(str);
